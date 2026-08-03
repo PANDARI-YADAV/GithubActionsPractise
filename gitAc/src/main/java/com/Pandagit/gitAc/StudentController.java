155e1b3751c75acd32b5572c1191fd3d7f7576d7
Pandari Yadav <137820847+PANDARI-YADAV@users.noreply.github.com>
2026-08-03 10:38:27 +0530

    Update CORS origin in StudentController

--- gitAc/src/main/java/com/Pandagit/gitAc/StudentController.java
+++ gitAc/src/main/java/com/Pandagit/gitAc/StudentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://34.229.89.158")
+@CrossOrigin(origins = "http://98.93.223.103")
 @RequestMapping("/students")
 public class StudentController {
 
