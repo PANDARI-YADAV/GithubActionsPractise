3bc487fbd9a717cf7f10d3516322ed23ad108bfa
Pandari Yadav <137820847+PANDARI-YADAV@users.noreply.github.com>
2026-08-04 09:47:25 +0530

    Change CORS origin in StudentController
    Updated CORS configuration to allow requests only from a specific origin.

--- gitAc/src/main/java/com/Pandagit/gitAc/StudentController.java
+++ gitAc/src/main/java/com/Pandagit/gitAc/StudentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://54.162.255.87")
 @RequestMapping("/students")
 public class StudentController {
 
